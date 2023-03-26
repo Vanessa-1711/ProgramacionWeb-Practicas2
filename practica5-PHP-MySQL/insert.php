c1065e624d917f01a6f1bb28e2e1083c67363df5
VanessaGarcia-1711 <vane171120@gmail.com>
2023-03-26 16:22:33 -0600

    practicas

--- practica5-PHP-MySQL/insert.php
+++ practica5-PHP-MySQL/insert.php
@@ -8,19 +8,13 @@
     <!-- Theme style -->
 </head>
 <body>
-<<<<<<< HEAD
     <h1>SAL ALGOOOO</h1>
-=======
-    <h3>AYUDAAAAAAAAAA</h3>
->>>>>>> e4b9268306bbc357d7961ddfea23608226353bfb
     <?php
         //conexion a ala base de datos 
         //nombre del servidor + nombre de usuario+contraseña + nombre de la base de datos
         $conexion=mysqli_connect("167.172.159.60", "admin", "5b8fc2b6176a8d1939f4b08c5724480d374f9b1603c1d563","formularios");
-        echo "entre php";
         // revisar conexion
         if($conexion==false){
-            echo ("ERROR");
             die("Error no existe una conexion a la base de datos".mysqli_connect_error());
         }
         //tomar los 5 valores del formulario a traves de los datos de los campos
@@ -28,10 +22,8 @@
         $nombre = $_REQUEST['nombre'];
         $precioVenta = $_REQUEST['precioven'];
         $precioCompra = $_REQUEST['precioComp'];
-        echo "asigne valores";
         //ejecutar el Query de insercion de datos
         $sql = "INSERT INTO productos VALUES('$codigo', '$nombre', '$precioVenta', '$precioCompra')";
-        echo "pase la insercion";
         //mandar mensaje de datos guardados
         $resultado = mysqli_query($conexion, $sql);
         if (!$resultado) {
